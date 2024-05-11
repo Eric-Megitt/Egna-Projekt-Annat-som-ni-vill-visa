18abeac24285ec23489ce2d0c762c5bc6c496c15
Eric-Megitt <108738723+Eric-Megitt@users.noreply.github.com>
2024-05-11 23:24:34 +0200

    Update Node.cs

--- BehaviourTreeStructure/Code/Node.cs
+++ BehaviourTreeStructure/Code/Node.cs
@@ -4,30 +4,19 @@ namespace BehaviourTree
 { 
 	public enum NodeState { RUNNING, SUCCESS, FAILURE }
 
-	public enum Result { NORMAL, INVERTED }
-
-	public class Node
-	{
-		protected List<Node> children;
-
-		private readonly Result result;
-
-
-		#region Constructors
-		internal Node(Result result = Result.NORMAL) => this.result = result;
-
-		internal Node(params Node[] children)
-		{
-			this.children = new();
-			foreach (Node child in children)
-				this.children.Add(child);
-		}
-		#endregion Constructors
-
-		protected virtual NodeState Evaluate() => NodeState.FAILURE; //gets overriden
-
-		public NodeState PerformNode() => Evaluate();
-
-		private static NodeState Invert(NodeState state) => (state == NodeState.RUNNING) ? state : (state == NodeState.SUCCESS) ? NodeState.FAILURE : NodeState.SUCCESS;
+	public class Node {  
+		protected List<Node> children;    
+		
+		internal Node(params Node[] children) {  
+			this.children = new();  
+			foreach (Node child in children)  
+				this.children.Add(child);  
+		}   
+		  
+		protected virtual NodeState Evaluate() {
+		    return NodeState.Failure; //gets overridden
+		} 
+	    
+		public NodeState PerformNode() => Evaluate(); 
 	}
 }
